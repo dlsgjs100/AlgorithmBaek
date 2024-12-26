292b908c47f86a8b072eda1584a5aa2079c3347a
dlsgjs100 <inkskku@gmail.com>
2024-12-26 10:37:33 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    for(int i = 0 ; i < numbers.size() ; i++){
+        answer += numbers[i];
+    }
+    answer /= (double)numbers.size();
+    return answer;
+}
