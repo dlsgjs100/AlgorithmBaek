90aa40883cf3382c9adcde64ac0516d04e80a272
dlsgjs100 <inkskku@gmail.com>
2025-04-24 09:59:44 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,47 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int blackJack(const vector<int>& cards , const int& M)
+int blackJack(const vector<int>& cards, const int& M)
 {
-    // 세장을 골라 합을 모두 구해 M보다 작거나 같으면서 최대값을 반환
     int count = cards.size();
     int maxSum = 0;
-    // 5
-    // 123 124 125 134 135 145 234 235 245 345
+    
     for(int i=0;i<count-2;i++)
     {
         for(int j=i+1;j<count-1;j++)
         {
-            for(int k=j+1;k<count;k++)
+            for(int k = j+1;k<count;k++)
             {
                 int sum = cards[i] + cards[j] + cards[k];
-                if(sum<=M && sum >=maxSum)
+                
+                if(sum<=M)
                 {
-                    maxSum = sum;
+                    maxSum = max(maxSum,sum);
+                    if(maxSum==M)
+                    {
+                        return maxSum;
+                    }
                 }
             }
         }
     }
     return maxSum;
 }
-
 int main()
 {
-    int N=0;
-    int M=0;
-    cin>>N;
-    cin>>M;
+    int N,M;
+    cin >> N >> M;
     
-    vector<int> cards;
+    vector<int> cards(N); 
     for(int i=0;i<N;i++)
     {
-        int num=0;
-        cin>>num;
-        cards.push_back(num);
+        cin >> cards[i];
     }
-    int answer = blackJack(cards,M);
-    cout<<answer;
+    cout<<blackJack(cards,M);
     return 0;
 }
