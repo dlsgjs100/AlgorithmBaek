bb3bda14318f8c2a5f969126e1a20ae2611e2a01
dlsgjs100 <inkskku@gmail.com>
2025-04-26 19:06:49 +0900

    [Silver V] Title: 수들의 합 5, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -4,33 +4,36 @@ using namespace std;
 
 int countSerialSum(const int& N)
 {
-    // 1부터 출발하여
-    // 연속된 자연수를 더하다가 N과 같아지면 방법++, 커지면 다음 숫자로, N까지
     int count = 0;
-    for (int i = 1; i <= N; i++)
+    int start = 1, end = 1;
+    int sum = 1;
+    while(start<=N)
     {
-        int sum = 0;
-        for (int j = i; j <= N; j++)
+        if(sum==N)
         {
-            sum += j;
-            if (sum > N)
-            {
-                break;
-            }
-            else if (sum == N)
-            {
-                count++;
-                break;
-            }
+            count++;
+            sum-=start;
+            start++;
+        }
+        else if(sum<N)
+        {
+            end++;
+            sum+=end;
+        }
+        else// sum > N
+        {
+            sum-=start;
+            start++;
         }
     }
-	return count;
+    
+    return count;
 }
 
 int main()
 {
     int N;
-    cin >> N;
-    cout << countSerialSum(N);
+    cin>>N;
+    cout<<countSerialSum(N);
     return 0;
 }
