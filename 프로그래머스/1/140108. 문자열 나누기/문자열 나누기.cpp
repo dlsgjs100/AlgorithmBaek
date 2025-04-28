6d42b134423518670f3c810f6bd649778dd6e845
dlsgjs100 <inkskku@gmail.com>
2025-04-28 10:41:48 +0900

    [level 1] Title: 문자열 나누기, Time: 0.06 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -5,32 +5,37 @@ using namespace std;
 
 int solution(string s) {
     int answer = 0;
-    int start=0;
-    
-    do{
-        int countX=0;
-        int countNotX=0;
+    int start = 0;
+
+    do {
+        int countX = 0;
+        int countNotX = 0;
         char x = s[start];
-        for(int i=start;i<s.size();i++){
-            if(s[i]==x){
+        for (int i = start; i < s.size(); i++) 
+        {
+            if (s[i] == x) 
+            {
                 countX++;
             }
-            else{
+            else 
+            {
                 countNotX++;
             }
-            if(countX==countNotX){
-                start = i+1;
+            if (countX == countNotX) 
+            {
+                start = i + 1;
                 answer++;
                 break;
             }
         }
-        
-        if (countX != countNotX) {
+
+        if (countX != countNotX) 
+        {
             answer++;
             break;
         }
-        
-    }while(start<s.size());
-    
+
+    } while (start < s.size());
+
     return answer;
 }
