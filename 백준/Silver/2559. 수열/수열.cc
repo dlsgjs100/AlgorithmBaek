f1db94053fa58c30bff8487e750c001a4e0fb120
dlsgjs100 <inkskku@gmail.com>
2025-08-21 14:20:38 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -6,32 +6,31 @@ using namespace std;
 
 int main()
 {
-
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N; // 2~100,000
-	int K; // 1~N
+	int N, K;
 	cin >> N >> K;
-
-	vector<int> temperatures(N + 1); // -100~100
-	vector<int> prefixSum(N + 1, 0); // 누적합 배열
-
+	vector<int> temperatures(N + 1);
 	for (int i = 1; i <= N; i++)
 	{
 		cin >> temperatures[i];
 	}
 
+	int window = 0;
 	for (int i = 1; i <= K; i++)
 	{
-		prefixSum[1] += temperatures[i]; // 첫 K개의 온도 합
+		window += temperatures[i];
 	}
+	int maxSum = window;
 
-	for (int i = 2; i <= N - K + 1; i++)
+	for (int i = K + 1; i <= N; i++)
 	{
-		prefixSum[i] = prefixSum[i - 1] - temperatures[i - 1] + temperatures[i + K - 1]; // 슬라이딩 윈도우 방식으로 누적합 계산
+		window += temperatures[i] - temperatures[i - K];
+		if (window > maxSum)
+		{
+			maxSum = window;
+		}
 	}
-
-	auto maxIt = max_element(prefixSum.begin() + 1, prefixSum.begin() +N - K + 2); // 최대 누적합 찾기
-	cout << *maxIt << '\n'; 
+	cout << maxSum << '\n';
 }
